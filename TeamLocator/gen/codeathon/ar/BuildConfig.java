798c9c7425a5e458ce9e4625878b31f598dd8406
Brian Priest <brian.priest@geocent.com>
2012-11-28 20:15:45 -0500

    let's go home

---
+++ TeamLocator/gen/codeathon/ar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package codeathon.ar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TeamLocator/src/codeathon/ar/locator/TeamLocatorMain.java
@@ -0,0 +1,1 @@
+package codeathon.ar.locator;
